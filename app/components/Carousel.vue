625f62cd5b48eeb88a982aa27cc11a2a5915a048
Mzer Emmanuel Terdoo <mzerterdoo6@gmail.com>
2024-09-13 19:44:04 +0100

    fixed carouse controllers

--- app/components/Carousel.vue
+++ app/components/Carousel.vue
@@ -1,7 +1,9 @@
 <template>
   <div v-if="showCarousel" id="carousel-container" class="py-52 relative overflow-hidden">
+    <h3 class="hidden min-755:block text-darkGreen italic font-bold text-xl absolute top-[15%] left-[30%]">
+      Aid Workers Braving Risks for a Safer World
+    </h3>
 
-    <h3 class=" hidden min-755:block text-darkGreen italic font-bold text-xl absolute top-[15%] left-[30%]">Aid Workers Braving Risks for a Safer World</h3>
     <!-- Carousel Images -->
     <div
       class="carousel-images flex"
@@ -17,7 +19,7 @@
           <img src="../assets/images/carousel/carousel_2_resized.jpg" alt="Image 2"
             class="w-full h-full rounded-full border-4 border-mainGreen object-cover" />
         </div>
-        <div class="relative lg:w-[320px] lg:h-[320px]  md:w-[250px] md:h-[250px] sm:w-[200px] sm:h-[200px] flex-shrink-0 -ml-6">
+        <div class="relative lg:w-[320px] lg:h-[320px] md:w-[250px] md:h-[250px] sm:w-[200px] sm:h-[200px] flex-shrink-0 -ml-6">
           <img src="../assets/images/carousel/carousel_3_resized.jpg" alt="Image 3"
             class="w-full h-full rounded-full border-4 border-mainGreen object-cover" />
         </div>
@@ -25,7 +27,7 @@
 
       <!-- Second set -->
       <div class="w-full flex justify-center min-w-full">
-        <div class="relative lg:w-[320px] lg:h-[320px] md:w-[250px] md:h-[250px] sm:w-[200px] sm:h-[200px] flex-shrink-0 ">
+        <div class="relative lg:w-[320px] lg:h-[320px] md:w-[250px] md:h-[250px] sm:w-[200px] sm:h-[200px] flex-shrink-0">
           <img src="../assets/images/carousel/carousel_4_resized.jpg" alt="Image 4"
             class="w-full h-full rounded-full border-4 border-mainGreen object-cover" />
         </div>
@@ -41,54 +43,51 @@
     </div>
 
     <!-- Previous Button -->
-    <svg
-    @click="prev"
-    :disabled="isPrevDisabled"
-    :class="[
-      isPrevDisabled ? 'border-lightGray' : 'border-mainGreen hover:bg-lightGreen', 
-      'hover:cursor-pointer absolute top-[42%] left-10 w-12 h-12 md:w-10 md:h-10 sm:w-8 sm:h-8 border-[3px] rounded-full bg-white'
-    ]"
-    viewBox="0 0 50 50"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M2 25C2 12.2974 12.2975 2 25 2C37.7026 2 48 12.2975 48 25C48 37.7025 37.7025 48 25 48C12.2975 48 2 37.7025 2 25Z"
-      fill="white"
-    />
-    <path
-      :stroke="isPrevDisabled ?  '#C2C2C2' : '#4CAF4F' "
-      d="M27.99 32.36L21.755 26.5704C20.7751 25.6605 20.7751 24.1095 21.755 23.1996L27.99 17.41"
-      stroke-width="3"
-      stroke-linecap="round"
-    />
-  </svg>
+   <!-- Previous Button -->
+<svg
+@click="prev"
+:disabled="isPrevDisabled"
+:class="[
+  isPrevDisabled ? 'border-lightGray' : 'border-mainGreen hover:bg-lightGreen', 
+  'hover:cursor-pointer absolute top-1/2 transform -translate-y-1/2 left-[3%] lg:left-[5%] w-12 h-12 md:w-10 md:h-10 sm:w-8 sm:h-8 border-[3px] rounded-full bg-white'
+]" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg"
+>
+<path
+  d="M2 25C2 12.2974 12.2975 2 25 2C37.7026 2 48 12.2975 48 25C48 37.7025 37.7025 48 25 48C12.2975 48 2 37.7025 2 25Z"
+  fill="white"
+/>
+<path
+  :stroke="isPrevDisabled ?  '#C2C2C2' : '#4CAF4F' "
+  d="M27.99 32.36L21.755 26.5704C20.7751 25.6605 20.7751 24.1095 21.755 23.1996L27.99 17.41"
+  stroke-width="3"
+  stroke-linecap="round"
+/>
+</svg>
 
-  <!-- Next Button -->
-  <svg
-    @click="next"
-    :disabled="isNextDisabled"
-    :class="[
-      isNextDisabled ? 'border-lightGray' : 'border-mainGreen hover:bg-lightGreen',
-      'hover:cursor-pointer absolute top-[42%] right-10 w-12 h-12 md:w-10 md:h-10 sm:w-8 sm:h-8 border-[3px] rounded-full bg-white'
-    ]"
-    viewBox="0 0 50 50"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M48 25C48 12.2974 37.7025 2 25 2C12.2974 2 2 12.2975 2 25C2 37.7025 12.2975 48 25 48C37.7025 48 48 37.7025 48 25Z"
-      fill="white"
-    />
-    <path
-      :stroke="isNextDisabled ?  '#C2C2C2' : '#4CAF4F' "
-      d="M22.01 32.36L28.245 26.5704C29.2249 25.6605 29.2249 24.1095 28.245 23.1996L22.01 17.41"
-      stroke-width="3"
-      stroke-linecap="round"
-    />
-  </svg>
+<!-- Next Button -->
+<svg
+@click="next"
+:disabled="isNextDisabled"
+:class="[
+  isNextDisabled ? 'border-lightGray' : 'border-mainGreen hover:bg-lightGreen',
+  'hover:cursor-pointer absolute top-1/2 transform -translate-y-1/2 right-[3%] lg:right-[5%] w-12 h-12 md:w-10 md:h-10 sm:w-8 sm:h-8 border-[3px] rounded-full bg-white'
+]" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg"
+>
+<path
+  d="M48 25C48 12.2974 37.7025 2 25 2C12.2974 2 2 12.2975 2 25C2 37.7025 12.2975 48 25 48C37.7025 48 48 37.7025 48 25Z"
+  fill="white"
+/>
+<path
+  :stroke="isNextDisabled ?  '#C2C2C2' : '#4CAF4F' "
+  d="M22.01 32.36L28.245 26.5704C29.2249 25.6605 29.2249 24.1095 28.245 23.1996L22.01 17.41"
+  stroke-width="3"
+  stroke-linecap="round"
+/>
+</svg>
+
   </div>
 </template>
+
 
 <script setup lang="ts">
 import { ref, computed, onMounted, onBeforeUnmount } from 'vue';
